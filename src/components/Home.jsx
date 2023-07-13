9b17ead7fc004b54fcdc12ee47edb819cf4cfb26
Bas Man <73193602+bas-man-dev@users.noreply.github.com>
2023-07-13 19:39:42 +0100

    start with useState

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,7 +1,18 @@
+import { useState } from "react";
+
 const Home = () => {
+  const [name, setName] = useState("Bas");
+  const [age, setAge] = useState(29);
+  const handleClick = () => {
+    setName("Bazza");
+    setAge(30)
+  };
+
   return (
     <div className="home">
       <h2>Home Page</h2>
+      <p>{name} is { age } years old</p>
+      <button onClick={handleClick}>Click me</button>
     </div>
   );
 };
